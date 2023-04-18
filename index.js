afa237f2739ff50a1b90f6a9305dd7a667b996ec
Cao Sang <caochisang1302@gmail.com>
2023-04-19 02:42:55 +0700

    update2

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const  express =require("express")
 const path=require("path")
 const app= express()
-app.use(express.static(path.join(__dirname+"/public")))
+// app.use(express.static(path.join(__dirname+"/public")))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
 const PORT=process.env.PORT || 5000
 
 const http=require("http")
