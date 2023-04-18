7efbf20023f9ec1f1eb8731f6f3f6701ccc78862
Cao Sang <caochisang1302@gmail.com>
2023-04-19 03:09:39 +0700

    update5

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const{Server}=require("socket.io")
 const io = new Server(server);
 
 // app.use(express.static(path.join(__dirname+"/public")))
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
