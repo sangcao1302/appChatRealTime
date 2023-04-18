6fb71b278dda4f4f93a408346be29a7c2bdfcc9c
Cao Sang <caochisang1302@gmail.com>
2023-04-19 03:20:59 +0700

    update6

--- index.js
+++ index.js
@@ -4,30 +4,30 @@ const app= express()
 const http=require("http")
 const server=http.createServer(app)
 const{Server}=require("socket.io")
-const io = new Server(server);
+// const io = new Server(server);
 
 // app.use(express.static(path.join(__dirname+"/public")))
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 const PORT=process.env.PORT || 5000
 
 
-// const cors=require("cors")
-// app.use(cors())
+const cors=require("cors")
+app.use(cors())
 
-// const io=new Server(server,{
-//     cors:{
-//         origin:"http://localhost:3000",
-//         methods:["GET","POST"],
+const io=new Server(server,{
+    cors:{
+        origin:"http://localhost:3000",
+        methods:["GET","POST"],
         
-//     }
-// })
+    }
+})
 
 
 
