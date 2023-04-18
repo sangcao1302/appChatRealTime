7f9e2255b58cf723b0be5c7315486cab9c94a41f
Cao Sang <caochisang1302@gmail.com>
2023-04-19 02:53:40 +0700

    update3

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 const  express =require("express")
 const path=require("path")
 const app= express()
+const http=require("http")
+const{Server}=require("socket.io")
+
+const server=http.createServer(app)
 // app.use(express.static(path.join(__dirname+"/public")))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 const PORT=process.env.PORT || 5000
 
-const http=require("http")
-const cors=require("cors")
-app.use(cors())
-const{Server}=require("socket.io")
 
-const server=http.createServer(app)
-const io=new Server(server,{
-    cors:{
-        origin:"http://localhost:3000",
-        methods:["GET","POST"],
+// const cors=require("cors")
+// app.use(cors())
+
+// const io=new Server(server,{
+//     cors:{
+//         origin:"http://localhost:3000",
+//         methods:["GET","POST"],
         
-    }
-})
-
+//     }
+// })
+const io = new Server(server);
 
 
 app.use(express.static(path.join(__dirname+"/public")))
