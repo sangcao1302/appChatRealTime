5508b5aa0fd35e450b63543fb26e1ca98eccd8a5
Cao Sang <caochisang1302@gmail.com>
2023-04-23 15:17:13 +0700

    update14

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const io=new Server(server,{
 
 
 
-// app.use(express.static(path.join(__dirname+"/public")))
+app.use(express.static(path.join(__dirname+"/public")))
 io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`)
    
